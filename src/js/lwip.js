df27b5a2004033ba921e15638c40c0a281487190
Conor Doyle <doyle7@hotmail.com>
2017-03-11 23:46:28 +0000

    resize pizza image

--- src/js/lwip.js
+++ src/js/lwip.js
@@ -2,7 +2,7 @@ var lwip = require('lwip');
 
 lwip.open('./src/views/images/pizzeria.jpg', function(err, image) {
   if (err) throw err;
-  image.resize(115.39, 75, function(err, image) {
+  image.resize(100, 75, function(err, image) {
     if (err) throw err;
     console.log('Image resized.');
     image.writeFile('./dist/views/images/pizzeria.jpg', function(err) {
